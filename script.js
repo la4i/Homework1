d9b6cb1435023610e001aa4a9254756462acbb92
*la4i* <*vlad_f_1723@mail.ru*>
2023-04-20 23:35:02 +0300

    Задание 8 из ДЗ 2.5

---
+++ script.js
@@ -0,0 +1,23 @@
+function seasons() {
+    let monthNumber = prompt('Напишите номер месяца');
+
+    if (monthNumber >= 1 && monthNumber <= 2 || monthNumber == 12) {
+        alert('Зима');
+    }
+
+    else if (monthNumber >= 3 && monthNumber <= 5) {
+        alert('Весна');
+    }
+
+    else if (monthNumber >= 6 && monthNumber <= 8) {
+        alert('Лето');
+    }
+
+    else if (monthNumber >= 9 && monthNumber <= 11) {
+        alert('Осень');
+    }
+
+    else if (monthNumber <=0 || monthNumber >=13) {
+        alert('Попробуй заново');
+    }
+}
