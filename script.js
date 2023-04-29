2fb9bdee36d52c154c39e10a56809e92c0e03c81
*la4i* <*vlad_f_1723@mail.ru*>
2023-04-29 21:23:37 +0300

    Доделал полностью верстку , добавил игру №2

--- script.js
+++ script.js
@@ -17,7 +17,42 @@ function seasons() {
         alert('Осень');
     }
 
-    else if (monthNumber <=0 || monthNumber >=13) {
+    else if (monthNumber <= 0 || monthNumber >= 13) {
         alert('Попробуй заново');
     }
 }
+
+function memorizethewords() {
+    const list = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+
+    alert('На экране будут представлены слова. Задача: запомнить слова и их порядок');
+    alert(list.sort(() => Math.random() - 0.5));
+
+    let answerUser = prompt('Какое слово было первым?');
+    let answerUser1 = prompt('Какое слово было последним?')
+
+    if (answerUser.toLowerCase() === list[0].toLowerCase() && answerUser1.toLowerCase() === list[list.length - 1].toLowerCase()) {
+        const win = confirm('Поздравляем с победой!');
+        if (win == true) {
+            memorizethewords();
+        } else {
+            alert('До встречи!');
+        }
+    } else if (answerUser.toLowerCase() === list[0].toLowerCase() || answerUser1.toLowerCase() === list[list.length - 1].toLowerCase()) {
+        const half = confirm('Вы были близки к победе!');
+        if (half == true) {
+            memorizethewords();
+        } else {
+            alert('До встречи!')
+        }
+    } else {
+        const loose = confirm('Вы ответили не верно');
+        if (loose == true) {
+            memorizethewords();
+        }  else {
+            alert('До встречи!')
+        }
+    }
+}
+
+
